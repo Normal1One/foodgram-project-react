c8682ed91e07ef2ddb25d14d213156fab08d0ffc
Normal_One <88935539+Normal1One@users.noreply.github.com>
2022-04-25 11:16:34 +0200

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,13 +124,13 @@ REST_FRAMEWORK = {
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'Russia/Moscow'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
