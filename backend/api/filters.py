ca67742af7c67e62a9c47dc3cfa199fc556cf602
N <N@spbnb-prc3306.t-systems.ru>
2022-04-27 09:25:05 +0200

    Filters fix 15

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -10,31 +10,3 @@ class IngredientsFilter(filters.FilterSet):
     class Meta:
         model = Ingredient
         fields = ('name', 'measurement_unit')
-
-
-class RecipesFilter(filters.FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(
-        method='get_favorite',
-        widget=BooleanWidget()
-    )
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart',
-        widget=BooleanWidget()
-    )
-
-    class Meta:
-        model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
-
-    def get_favorite(self, queryset, name, value):
-        user = self.request.user
-        if value:
-            return Recipe.objects.filter(favorites__user=user)
-        return Recipe.objects.all()
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        user = self.request.user
-        if value:
-            return Recipe.objects.filter(shoppingcart__user=user)
-        return Recipe.objects.all()
